568b4c6b57f1fe1e0568b89df5787971c85ab182
Tangent Chang <andy23512@gmail.com>
2018-12-25 12:58:01 +0800

    enable hmr in webpack dev

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import nav from "./nav";
 import { footer } from "./footer";
 import makeButton from "./button";
 import { makeColorStyle } from "./button-styles";
+import css from "./footer.css"
 
 const button = makeButton("Yay! A Button!");
 button.style = makeColorStyle("cyan");
