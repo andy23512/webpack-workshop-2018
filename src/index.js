b1a192b4602794c5f6785ca234683ed016ec1daf
Tangent Chang <andy23512@gmail.com>
2018-12-25 13:19:59 +0800

    add makeImage function, add url-loader to webpack config

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import nav from "./nav";
 import { footer } from "./footer";
 import makeButton from "./button";
 import { makeColorStyle } from "./button-styles";
+import makeImage from './image'
 
 import css from "./footer.css";
 import buttonStyles from "./button.css";
@@ -10,3 +11,7 @@ const button = makeButton("Yay! A Button!");
 button.style = makeColorStyle("cyan");
 document.body.appendChild(button);
 document.body.appendChild(footer);
+
+import imageURL from "./webpack-logo.jpg";
+const image = makeImage(imageURL);
+document.body.appendChild(image);
