8a7af344df09ac1a613711380b5fb6fc9433efc3
Tangent Chang <andy23512@gmail.com>
2018-12-25 13:09:16 +0800

    add mini-css-extract-plugin to webpack.production.js, add button.css

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { footer } from "./footer";
 import makeButton from "./button";
 import { makeColorStyle } from "./button-styles";
 import css from "./footer.css"
+import buttonStyles from "./button.css";
 
 const button = makeButton("Yay! A Button!");
 button.style = makeColorStyle("cyan");
